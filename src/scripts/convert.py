8123c94076427a5e2c88350a2f16372e762ed712
Joshua Klein <mobiusklein@gmail.com>
2022-02-20 20:24:04 -0500

    Add automatic release system to generate newstyle OBO and OWL

--- src/scripts/convert.py
+++ src/scripts/convert.py
@@ -1,4 +1,8 @@
 import argparse
+import difflib
+
+from typing import Set
+
 import fastobo
 
 datatypes = {
@@ -23,6 +27,25 @@ with open(args.input) as f:
     contents = f.read()
     doc = fastobo.loads(contents)
 
+
+def check_spelling(ref: str, words: Set[str]) -> str:
+    if ref in words:
+        return ref
+    nearest = difflib.get_close_matches(ref, words, n=2)
+    if not nearest:
+        print(f"Adding {ref} to word list")
+        words.add(ref)
+        return ref
+    if len(nearest) > 1:
+        print(f"{ref} is close to {nearest}, cannot discriminate, choosing {nearest[0]}")
+    print(f"Rewriting {ref} to {nearest[0]}")
+    return nearest[0]
+
+
+words = {'Origin', 'DiffAvg', 'Unimod', 'MassAvg', 'GNOme', 'MassMono', 'Formula',
+         'DiffFormula', 'Remap', 'Source', 'UniProt', 'TermSpec', 'FormalCharge', 'DiffMono'}
+
+
 for frame in doc:
 
     for i, clause in reversed(list(enumerate(frame))):
@@ -32,21 +55,23 @@ for frame in doc:
 
     for i, clause in enumerate(frame):
         if isinstance(clause, fastobo.term.XrefClause):
-            property = clause.xref.id.prefix
+            prop = check_spelling(clause.xref.id.prefix, words)
             value = clause.xref.desc
 
-            if property in {"UniProt", "Unimod", "GNOme"}:
+            if prop in {"UniProt", "Unimod", "GNOme"}:
                 xref = fastobo.xref.Xref(fastobo.id.parse(value))
                 new_clause = fastobo.term.XrefClause(xref)
-            elif property == "Remap":
+            elif prop == "Remap":
                 replacement = fastobo.id.parse(value)
                 new_clause = fastobo.term.ReplacedByClause(replacement)
             else:
-                rel = fastobo.id.UnprefixedIdent(property)
-                pv = fastobo.pv.LiteralPropertyValue(rel, value, datatypes[property]) 
+                rel = fastobo.id.UnprefixedIdent(prop)
+                pv = fastobo.pv.LiteralPropertyValue(rel, value, datatypes[prop])
                 new_clause = fastobo.term.PropertyValueClause(pv)
-           
+
             frame[i] = new_clause
-            
+
+print(f"Final Word List: {words}")
+
 with open(args.output, "w") as dst:
-    print(doc, file=dst)
+    dst.write(str(doc))
